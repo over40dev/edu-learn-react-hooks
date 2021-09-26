bbc9aea506373a0b1831fc264daa8eb2a8803fec
Cogentx <code@cogentx.com>
2021-09-25 21:49:11 -0230

    Async State inside Class Component

--- src/components/CounterStateClass.js
+++ src/components/CounterStateClass.js
@@ -12,29 +12,34 @@ export default class CounterStateClass extends Component {
     };
   }
 
+  // option 1 - include as a class method
   increment() {
-    this.setState({
-      count1: this.state.count1 + 1,
-      count2: {value: this.state.count2.value + 1},
-    });
+    this.setState((previousState) => ({
+      count1: previousState.count1 + 1,
+      count2: {
+        value: previousState.count2.value + 1,
+      },
+    }));
   }
 
-  decrement() {
-    this.setState({
-      count1: this.state.count1 - 1,
-      count2: {value: this.state.count2.value - 1},
-    });
+  // option 2 - include as a property set to an arrow function
+  decrement = () => {
+    this.setState((previousState) => ({
+      count1: previousState.count1 - 1,
+      count2: {value: previousState.count2.value - 1},
+    }));
   }
 
   render() {
-    console.log(this);
     return (
       <div>
         <h2>State Class</h2>
         <h3>Count1: {this.state.count1}</h3>
         <h3>Count2: {this.state.count2.value}</h3>
+        {/* option 1 - call class method using arrow function */}
         <button onClick={() => this.increment()}>+</button>
-        <button onClick={() => this.decrement()}>-</button>
+        {/* option 2 - call class property set to a method by referencing method by name*/}
+        <button onClick={this.decrement}>-</button>
       </div>
     );
   }
